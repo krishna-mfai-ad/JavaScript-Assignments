351c12b23d2581d75cb781e804ea585a728757fb
Krishna R <164004602+krishna-mfai-ad@users.noreply.github.com>
2024-04-26 12:34:20 +0530

    Add files via upload

---
+++ JavaScript-Test/Uppercase.js
@@ -0,0 +1,4 @@
+var given="bala"
+var n=given.charAt(0).toUpperCase()
+var ans=n+given.substring(1,given.length)
+console.log(ans)
---
+++ JavaScript-Test/arithmeticOperations.js
@@ -0,0 +1,23 @@
+var a=30
+var b=15
+function add(a,b){
+    console.log(a+b)
+}
+function sub(a,b){
+    console.log(a-b)
+}
+function mul(a,b){
+    console.log(a*b)
+}
+function div(a,b){
+    console.log(a/b)
+}
+add(a,b)
+
+sub(a,b)
+
+mul(a,b)
+
+div(a,b)
+
+
