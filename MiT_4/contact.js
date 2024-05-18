bc5f2013bc64807733560ba96a98a6b48f86e388
Krishna R <164004602+krishna-mfai-ad@users.noreply.github.com>
2024-05-18 11:32:45 +0530

    Add files via upload

---
+++ MiT_4/contact.js
@@ -0,0 +1,62 @@
+var submitBtn = document.getElementById("submitBtn")
+var displayData = document.getElementById("displayData")
+var outBox = document.getElementById("outBox")
+var dataContainer=document.getElementById("dataContainer")
+
+function displayDatas() {
+    // Clear the container first
+    dataContainer.innerHTML = '';
+
+    let list = JSON.parse(localStorage.getItem('datas')) || [];
+
+    list.forEach(function(item, index) {
+        let div = document.createElement('div');
+        div.innerHTML = 
+        `
+        <div id=outBox class="output-box">
+        <div>
+        <span>${name}</span>
+        <p>${mail}</p>
+        </div>
+        <button onclick="deleteData()"> Delete </button>
+        </div >
+        `;
+        dataContainer.appendChild(div);
+    });
+}
+
+// Push Data in Local Storage
+submitBtn.addEventListener('click', function () {
+    let name = document.querySelector("#conName").value
+    let mail = document.querySelector('#email').value
+
+    let contacts = JSON.parse(localStorage.getItem('datas')) || []
+
+    contacts.push({
+        "Name": name, "Email": mail
+    })
+
+    localStorage.setItem("datas", JSON.stringify(contacts))
+    displayDatas();
+})
+
+ function deleteData()
+ {
+    displayData.innerHTML=''  
+ }
+ 
+//  window.deleteData = function(index) {
+//     let list = JSON.parse(localStorage.getItem('dataset')) || [];
+
+//     list.splice(index, 1);
+
+//     localStorage.setItem("dataset", JSON.stringify(list));
+
+//     displayData();
+// }
+
+
+
+
+
+
